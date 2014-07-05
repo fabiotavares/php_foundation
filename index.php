aa08c860488f15712b3841e9a38f5380f59487d1
Fabio Tavares <fabreder@gmail.com>
2014-07-05 12:10:45 -0300

    Realizando correcoes de acesso ao index.php

--- index.php
+++ index.php
@@ -11,7 +11,13 @@
     <body>
     <div>
         <?php
-            $pagina = $_GET["page"].".php";
+            //armazena a página que deseja acessar
+            if(! isset($_GET["page"])){
+                $pagina = "home.php";
+            } else{
+                $pagina = $_GET["page"].".php";
+            }
+            //verifica se a página existe no sistema
             if(file_exists($pagina))
             {
                 require_once($pagina);
--- menu.php
+++ menu.php
@@ -2,11 +2,16 @@
     <div class="navbar-inner">
         <a class="brand" href="#">Site Simples</a>
         <ul class="nav">
-            <li <?php if($_GET["page"] == "home") echo "class='active'"; ?>><a href="index.php?page=home">Home</a></li>
-            <li <?php if($_GET["page"] == "empresa") echo "class='active'"; ?>><a href="index.php?page=empresa">Empresa</a></li>
-            <li <?php if($_GET["page"] == "produtos") echo "class='active'"; ?>><a href="index.php?page=produtos">Produtos</a></li>
-            <li <?php if($_GET["page"] == "servicos") echo "class='active'"; ?>><a href="index.php?page=servicos">Serviços</a></li>
-            <li <?php if($_GET["page"] == "contato") echo "class='active'"; ?>><a href="index.php?page=contato">Contato</a></li>
+            <li <?php if((! isset($_GET["page"])) or (isset($_GET["page"]) and $_GET["page"] == "home")) echo "class='active'"; ?>>
+                <a href="index.php?page=home">Home</a></li>
+            <li <?php if(isset($_GET["page"]) and $_GET["page"] == "empresa") echo "class='active'"; ?>>
+                <a href="index.php?page=empresa">Empresa</a></li>
+            <li <?php if(isset($_GET["page"]) and $_GET["page"] == "produtos") echo "class='active'"; ?>>
+                <a href="index.php?page=produtos">Produtos</a></li>
+            <li <?php if(isset($_GET["page"]) and $_GET["page"] == "servicos") echo "class='active'"; ?>>
+                <a href="index.php?page=servicos">Serviços</a></li>
+            <li <?php if(isset($_GET["page"]) and $_GET["page"] == "contato") echo "class='active'"; ?>>
+                <a href="index.php?page=contato">Contato</a></li>
         </ul>
     </div>
 </div>
